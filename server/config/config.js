a7d29aa8a8e6f37e60200a986b239dd311a92b43
“Vin” <vinspecter@gmail.com>
2020-02-22 19:13:02 -0300

    first commit

---
+++ server/config/config.js
@@ -0,0 +1,7 @@
+//process es una aplicacion que esta siempre corriendo en esta aplicaion
+// lo que quiero es modificar esta variable
+//============================
+//      Puerto
+//============================
+
+process.env.PORT = process.env.PORT || 3000; // si no existe la variable port entonces sera igual a 3000
